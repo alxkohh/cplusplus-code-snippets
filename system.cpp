6f38a8fb3ff83b4228566100b318a1c839b2b053
Alex <alxkohh@gmail.com>
2020-09-22 23:38:54 +0800

    Add system

---
+++ system.cpp
@@ -0,0 +1,24 @@
+/*
+The system() function allows us to run bash/linux commands.
+With the help of system(), we can write C++ programs that replace bash scripts or even
+extend bash scripts, since we can utilise C++ features that do not exist in bash scripting.
+
+Basically, the argument passed to the system() function is the command you want to execute
+on your command line.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+char* toCharPtr(string s) {
+    return &*s.begin();
+}
+
+int main() {
+    string lsCommand = "ls";
+    system(toCharPtr(lsCommand));
+
+    // same effect as typing 'ls' on cmd line
+
+    return 0;
+}
